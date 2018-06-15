6f57f5d1e9a45310c66ed4cfb49994b28bc0f8af
Vgupta <vinay.gupta.contractor@move.com>
2018-06-15 16:56:58 +0530

    employee api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_091033) do
+ActiveRecord::Schema.define(version: 2018_06_15_112433) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2018_06_15_091033) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "salalry"
+    t.integer "rating"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
